050bfe8ece63b59bc4f9c9aa03abd878c5106ede
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2023-04-05 12:08:18 +0300

    border and square created

--- src/maziez/models/border.py
+++ src/maziez/models/border.py
@@ -7,3 +7,20 @@ class Border(IntFlag):
     BOTTOM = auto()
     LEFT = auto()
     RIGHT = auto()
+
+    @property
+    def corner(self) -> bool:
+        return self in (
+            self.TOP | self.LEFT,
+            self.TOP | self.RIGHT,
+            self.BOTTOM | self.LEFT,
+            self.BOTTOM | self.RIGHT,
+        )
+
+    @property
+    def dead_end(self) -> bool:
+        return self.bit_count() == 3
+
+    @property
+    def intersection(self) -> bool:
+        return self.bit_count() < 2
---
+++ src/maziez/models/square.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from maziez.models.border import Border
+from maziez.models.role import Role
+
+
+@dataclass(frozen=True)
+class Square:
+    index: int
+    row: int
+    column: int
+    border: Border
+    role: Role = Role.NONE
